889b06e8ed334416a0b0dc42f9dac01635e34fef
Wiiilmaa <github@jenswilmer.de>
2023-11-17 21:55:40 +0100

    cors endpoint

--- backend/app/api/game_endpoints.py
+++ backend/app/api/game_endpoints.py
@@ -20,7 +20,7 @@ from flask_socketio import SocketIO
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
 async_mode = "gevent"
-socketio = SocketIO(app, async_mode=async_mode)
+socketio = SocketIO(app, async_mode=async_mode, cors_allowed_origins="*")
 
 
 # Get User from Game
